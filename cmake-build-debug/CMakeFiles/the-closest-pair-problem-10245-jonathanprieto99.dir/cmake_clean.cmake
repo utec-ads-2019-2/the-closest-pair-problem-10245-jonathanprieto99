53a40138c0067bb6a6528fcf3bec3430ccc3ad2c
Jonathan Prieto <jonathan.prieto@me.com>
2019-08-31 14:22:34 -0500

    first upload

---
+++ cmake-build-debug/CMakeFiles/the-closest-pair-problem-10245-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/the-closest-pair-problem-10245-jonathanprieto99.dir/main.cpp.o"
+  "the-closest-pair-problem-10245-jonathanprieto99.pdb"
+  "the-closest-pair-problem-10245-jonathanprieto99"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/the-closest-pair-problem-10245-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/the-closest-pair-problem-10245-jonathanprieto99.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for the-closest-pair-problem-10245-jonathanprieto99.
+# This may be replaced when dependencies are built.
